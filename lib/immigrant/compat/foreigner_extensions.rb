09e1c66ff235e93be0ea5d457560b112f043bdf3
Jon Jensen <jenseng@gmail.com>
2015-02-23 00:07:31 -0700

    ruby < 2.1 fix

--- lib/immigrant/compat/foreigner_extensions.rb
+++ lib/immigrant/compat/foreigner_extensions.rb
@@ -5,7 +5,7 @@ module Immigrant
     def self.included(klass)
       # ForeignKeyExtensions already overrides initialize; override it
       # some more
-      klass.include(Module.new{
+      klass.send(:include, Module.new{
         def initialize(from_table, to_table, options)
           options.delete(:on_update)
           options[:dependent] = normalize_dependent(options.delete(:on_delete))
